80513e01d86eed7a8217ef30d49a9ee6e3ac037f
alanwgeorge <ageorge@alan-george.com>
2016-08-06 14:23:07 -0700

    init

---
+++ src/java/com/alangeorge/algotithums/one/Helloworld.java
@@ -0,0 +1,10 @@
+package com.alangeorge.algotithums.one;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Helloworld {
+
+    public static void main(String[] args) {
+        StdOut.print("helloword");
+    }
+}
